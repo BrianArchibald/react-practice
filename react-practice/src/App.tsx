ecb414768310d198cbb0ce2f8f460b8530898440
Brian Archibald <brianallenarchibald@gmail.com>
2020-12-15 17:20:36 -0600

    Refactor: Setting up API to call

--- react-practice/src/App.tsx
+++ react-practice/src/App.tsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
 
     const [ counter, setCounter ] = useState(0)
+
+    const fetchAPIData = () => {
+        return axios.get('https://dog.ceo/api/breeds/image/random')
+          .then(data => {
+            // handle success
+            console.log(data);
+            return data.data.message;
+          })
+          .catch(err => {
+            console.log(err);
+          })
+    }
 
   return (
     <div className="App">
@@ -14,14 +27,21 @@ const App = () => {
           <button onClick={ () => {
           setCounter(counter + 1);
           }}> Add 1 </button>
+
           <br></br>
           <button onClick={ () => {
           setCounter(0);
           }}> Reset </button>
 
+          <br></br>
+          <button onClick={ () => {
+          fetchAPIData();
+          }}>Dogs!!  </button>
+
       </header>
     </div>
   );
 }
+
 
 export default App;
